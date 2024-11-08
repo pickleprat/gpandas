f5a6403124fc8a82b157875357226542762cd3bd
apoplexi24 <shivanandanasharma@gmail.com>
2024-11-08 14:10:01 +0530

    Improved documentation in utils/collection/set.go... Added conditions to checj in DataFrame.rename()

--- gpandas.go
+++ gpandas.go
@@ -1,35 +1,45 @@
 package gpandas
 
 import (
+	"errors"
 	"gpandas/dataframe"
-	"gpandas/utils/collection"
 )
 
-type float_column struct {
-	vals []float64
-}
+// FloatColumn represents a slice of float64 values.
+type FloatColumn []float64
 
-type string_column struct {
-	vals []string
-}
+// StringColumn represents a slice of string values.
+type StringColumn []string
 
-type int_column struct {
-	vals []int64
-}
+// IntColumn represents a slice of int64 values.
+type IntColumn []int64
 
-func (gpandas) DataFrame(columns []string, data map[string][]interface{}) (*dataframe.DataFrame, error) {
+// BoolColumn represents a slice of bool values.
+type BoolColumn []bool
+
+// Column represents a slice of any type.
+type Column []any
+
+// TypeColumn represents a slice of a comparable type T.
+type TypeColumn[T comparable] []T
+
+func (gpandas) DataFrame(columns []string, data map[string][]any, columns_types map[string]any) (*dataframe.DataFrame, error) {
+	if columns_types == nil {
+		return nil, errors.New("provide columns_type map to assert types to the columns")
+	}
+
+	if columns != nil && data == nil {
+
+	} else if columns == nil && data != nil {
+
+	}
 	cols, err := dataframe.GetMapKeys(data)
 	if err != nil {
 		return nil, err
-	}
-	cols_compare, err2 := collection.NewSet[string]()
-	if err2 != nil {
-		return nil, err2
 	}
 
 	df := &dataframe.DataFrame{data: data}
 	return df, nil
 }
 
-type gpandas struct {
-}
+type gpandas struct{}
--- utils/collection/set.go
+++ utils/collection/set.go
@@ -34,7 +34,10 @@ func (s Set[T]) Add(v T) error {
 	return errors.New("value already exists in the set")
 }
 
-// AddMulti adds multiple values to the set at once.
+// AddMulti inserts multiple values into the set only if they do not already exist.
+// It iterates over the provided values and attempts to add each one to the set.
+// If any value already exists in the set, the function returns an error.
+// If all values are successfully added, the function returns nil.
 func (s Set[T]) AddMulti(val ...T) error {
 	for _, v := range val {
 		err := s.Add(v)
@@ -48,7 +51,10 @@ func (s Set[T]) AddMulti(val ...T) error {
 // FilterFunc defines a function type that returns true if a value should be included in filtered results.
 type FilterFunc[T any] func(v T) bool
 
-// Filter creates a new Set containing only the elements that satisfy the predicate P.
+// Filter applies a filter function to each element of the set and returns a new set containing only the elements that pass the filter.
+// The filter function P is applied to each element of the set. If the function returns true for an element, it is included in the resulting set.
+// If the function returns false, the element is not included.
+// The function returns a new Set containing the filtered elements and an error if the operation fails.
 func (s Set[T]) Filter(P FilterFunc[T]) (Set[T], error) {
 	res, err := NewSet[T]()
 	if err != nil {
@@ -63,7 +69,16 @@ func (s Set[T]) Filter(P FilterFunc[T]) (Set[T], error) {
 	return res, nil
 }
 
-// Union creates a new Set containing all elements from both sets.
+// Union creates a new Set containing all elements from both the current set and the input set.
+// It iterates over the elements of both sets and adds them to a new Set, ensuring no duplicates.
+// The resulting Set contains all unique elements from both input sets in an arbitrary order.
+//
+// Parameters:
+//   s2: The input Set[T] to union with the current set.
+//
+// Returns:
+//   A Set of type Set[T] containing all unique elements from both input sets.
+//   An error if the operation fails.
 func (s Set[T]) Union(s2 Set[T]) (Set[T], error) {
 	res, err := NewSet[T]()
 	if err != nil {
@@ -95,6 +110,15 @@ func ToSlice[T comparable](s Set[T]) ([]T, error) {
 	return result_slice, nil
 }
 
+// ToSet converts a slice of type []T into a Set of type Set[T].
+// It iterates over the elements of the slice and adds them to a Set.
+// The resulting Set contains all the elements from the input slice in an arbitrary order.
+//
+// Parameters:
+//   slice: A slice of type []T from which to create the Set.
+//
+// Returns:
+//   A Set of type Set[T] containing all elements from the input slice.
 func ToSet[T comparable](slice []T) (Set[T], error) {
 	result_set, err := NewSet[T]()
 	if err != nil {
@@ -106,7 +130,17 @@ func ToSet[T comparable](slice []T) (Set[T], error) {
 	return result_set, nil
 }
 
-// Intersect creates a new Set containing only elements that exist in both sets.
+// Intersect creates a new Set containing elements that exist in both the current set and the input set.
+// It iterates over the elements of the current set and checks if they exist in the input set.
+// If an element exists in both sets, it is added to the resulting set.
+// The resulting Set contains all the elements that are common to both input sets in an arbitrary order.
+//
+// Parameters:
+//   s2: The input Set[T] to intersect with the current set.
+//
+// Returns:
+//   A Set of type Set[T] containing all elements that are common to both input sets.
+//   An error if the operation fails.
 func (s Set[T]) Intersect(s2 Set[T]) (Set[T], error) {
 	res, err := NewSet[T]()
 	if err != nil {
@@ -121,7 +155,17 @@ func (s Set[T]) Intersect(s2 Set[T]) (Set[T], error) {
 	return res, nil
 }
 
-// Difference creates a new Set containing elements that exist in the first set but not in the second.
+// Difference creates a new Set containing elements that exist in the current set but not in the input set.
+// It iterates over the elements of the current set and checks if they do not exist in the input set.
+// If an element does not exist in the input set, it is added to the resulting set.
+// The resulting Set contains all the elements that are unique to the current set in an arbitrary order.
+//
+// Parameters:
+//   s2: The input Set[T] to find the difference with the current set.
+//
+// Returns:
+//   A Set of type Set[T] containing all elements that are unique to the current set.
+//   An error if the operation fails.
 func (s Set[T]) Difference(s2 Set[T]) (Set[T], error) {
 	res, err := NewSet[T]()
 	if err != nil {
@@ -136,6 +180,18 @@ func (s Set[T]) Difference(s2 Set[T]) (Set[T], error) {
 	return res, nil
 }
 
+// Compare checks if two sets are equal by comparing their lengths and ensuring all elements in the first set exist in the second set.
+// It first checks if the lengths of the two sets are equal. If not, it returns false and nil.
+// Then, it iterates over the elements of the first set and checks if each element exists in the second set.
+// If an element does not exist in the second set, it returns false and the non-existent element.
+// If all elements in the first set exist in the second set, it returns true and nil.
+//
+// Parameters:
+//   s2: The input Set[T] to compare with the current set.
+//
+// Returns:
+//   A boolean indicating if the sets are equal.
+//   An any value representing the first element that does not exist in the second set if the sets are not equal, or nil if they are equal.
 func (s Set[T]) Compare(s2 Set[T]) (bool, any) {
 	// Check if lengths are the same
 	if len(s) != len(s2) {
